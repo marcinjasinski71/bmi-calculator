828d5651fb167d12fd83affec99ead70beae193f
Marcin Jasiński <alpinus1@gmail.com>
2022-04-04 13:59:20 +0200

    quickfix

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-// jedna funkcja która będzie nam liczyła nasze bmi
-
 function Calculate() {
 	const height = document.getElementById('height-input').value;
 	const weight = document.getElementById('weight-input').value;
-	// pobieramy wartość z inputów z id height-input i weight-input
 
-	// wzór - result to number Weight podzielony przez number Height/100 i wzięty do kwadratu (wynik BMI)
 	const result = (parseFloat(weight) / (parseFloat(height) / 100) ** 2).toFixed(
 		1
 	);
 
 	if (!isNaN(result)) {
 		document.getElementById('bmi-output').innerHTML = result;
-		//wynik naszego BMI jest wypisywany jako RESULT do bmi-output(tam gdzie value)
 
 		if (result < 18.5) {
 			document.getElementById('bmi-status').innerHTML = 'Underweight 💀';
